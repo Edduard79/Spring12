bc0854b0429fdf20789dc4bc1a85cd1d10820720
Edduard79 <eduard.22@gmail.com>
2023-05-15 15:58:36 +0200

    First Try

---
+++ src/main/java/Ex12/CarController.java
@@ -0,0 +1,72 @@
+package Ex11;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+    @Autowired
+    private CarRepo carRepo;
+
+
+    @PostMapping
+    public ResponseEntity<Car> createCar(@RequestBody Car car) {
+        Car savedCar = carRepo.save(car);
+        return ResponseEntity.created(URI.create("/cars/" + savedCar.getId())).body(savedCar);
+    }
+
+    @GetMapping
+    public List<Car> getCars() {
+        return carRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> getOneById(@PathVariable Long id) {
+        Optional<Car> optionalCar = carRepo.findById(id);
+        if (optionalCar.isPresent()) {
+            return ResponseEntity.ok(optionalCar.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Car> updateCarType(@PathVariable Long id, @RequestBody Car car) {
+        Optional<Car> optionalCar = carRepo.findById(id);
+        if (optionalCar.isPresent()) {
+            Car existingCar = optionalCar.get();
+            existingCar.setType(car.getType());
+            return ResponseEntity.ok(carRepo.save(existingCar));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Car> deleteCar(@PathVariable Long id) {
+        Optional<Car> optionalCar = carRepo.findById(id);
+        if (optionalCar.isPresent()) {
+            carRepo.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Car> deleteAll() {
+        carRepo.deleteAll();
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
