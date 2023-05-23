18fdd104258e0600f4105c3601c311e6b41562b6
Edduard79 <eduard.22@gmail.com>
2023-05-23 17:13:01 +0200

    Hope it works2

--- src/main/java/Ex12/CarController.java
+++ src/main/java/Ex12/CarController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
