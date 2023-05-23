8a09cebdd90a70b74481d6ed55d204cb6b72e66e
Edduard79 <eduard.22@gmail.com>
2023-05-23 17:01:22 +0200

    Third Try

--- src/main/java/Ex12/CarController.java
+++ src/main/java/Ex12/CarController.java
@@ -3,7 +3,15 @@ package Ex12;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 import java.net.URI;
@@ -40,11 +48,11 @@ public class CarController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Car> updateCarType(@PathVariable Long id, @RequestParam String type) {
+    public ResponseEntity<Car> updateCarType(@PathVariable Long id, @RequestBody Car car) {
         Optional<Car> optionalCar = carRepo.findById(id);
         if (optionalCar.isPresent()) {
             Car existingCar = optionalCar.get();
-            existingCar.setType(type);
+            existingCar.setType(car.getType());
             return ResponseEntity.ok(carRepo.save(existingCar));
         } else {
             return ResponseEntity.notFound().build();
--- src/main/java/Ex12/Ex12Application.java
+++ src/main/java/Ex12/Ex12Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ex12Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ex12Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ex12Application.class, args);
+    }
 
 }
