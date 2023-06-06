b31287f214626386917796652daa03fccf11ed78
Edduard79 <eduard.22@gmail.com>
2023-06-06 14:54:50 +0200

    Hope it works7

--- src/main/java/exercise/twelve/CarController.java
+++ src/main/java/exercise/twelve/CarController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.net.URI;
